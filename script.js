2ee15494b304d54d3b855d594910b345972a8df1
Algirdas83 <algirdasliutkus@gmail.com>
2023-01-05 16:25:21 +0200

    smoof navigating frue nav bar

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -9,6 +7,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to')
 const section1 = document.querySelector('#section--1')
+
+///////////////////////////////////////
+// Modal window
+
+
 
 const openModal = function (e) {
   e.preventDefault()
@@ -36,6 +39,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Button scrolling
 btnScrollTo.addEventListener('click', (e) => {
   e.preventDefault()
   
@@ -43,9 +47,21 @@ btnScrollTo.addEventListener('click', (e) => {
 })
 
 
+document.querySelector('.nav__links').addEventListener('click', function(e){
+ 
+  e.preventDefault()
+  console.log(e.target);
+  //Matching strategy (event delegation)
+  if(e.target.classList.contains('nav__link')){
+   
+       const id = e.target.getAttribute('href')
+       document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
 
-/////
+/////////////////////////////////////////////////////
 
+ 
 
 
 
