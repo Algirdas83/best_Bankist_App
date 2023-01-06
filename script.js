da0419a3fdea823cc8e6bcb2143e10326f70a536
Algirdas83 <algirdasliutkus@gmail.com>
2023-01-06 17:38:17 +0200

    added Sticky navigation func

--- script.js
+++ script.js
@@ -107,6 +107,27 @@ nav.addEventListener('mouseover', handleNavFaid.bind(0.5))
 
 nav.addEventListener('mouseout', handleNavFaid.bind(1))
 
+
+// Sticky navigation : Intersection Observer API  
+
+const header = document.querySelector('.header')
+const navHeight = nav.getBoundingClientRect().height
+
+
+const stickyNav = function(entries){
+  const [entry ] = entries
+  
+  
+  if(!entry.isIntersecting) {
+    nav.classList.add('sticky')
+  }else{
+    nav.classList.remove('sticky')
+  }
+}
+
+const headerObv = new IntersectionObserver(stickyNav,{root: null,  threshold: 0, rootMargin: `-${navHeight}px` } )
+
+headerObv.observe(header)
+
 /////////////////////////////////////////////////////
 
- 
