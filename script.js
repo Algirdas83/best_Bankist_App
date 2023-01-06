214b9b11cbb764f3d1f0db32a2a2ef12c948b212
Algirdas83 <algirdasliutkus@gmail.com>
2023-01-06 09:08:12 +0200

    section 2 tab functionality

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ btnScrollTo.addEventListener('click', (e) => {
 document.querySelector('.nav__links').addEventListener('click', function(e){
  
   e.preventDefault()
-  console.log(e.target);
+  
   //Matching strategy (event delegation)
   if(e.target.classList.contains('nav__link')){
    
@@ -59,9 +59,31 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
   }
 })
 
+
+//Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', (e) => {
+  
+  const clicked = e.target.closest('.operations__tab')
+  
+  //Guard clause
+  if(!clicked) return
+// Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'))
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+
+//Activate tab
+  clicked.classList.add('operations__tab--active')
+
+  //Active content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+  
+})
+
 /////////////////////////////////////////////////////
 
  
-
-
-
